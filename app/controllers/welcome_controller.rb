407d98e99f8db570abb291b316d7d1e1d3e1ee4e
SheaMeyers <shea.meyers@gmail.com>
2016-03-11 22:04:43 -0600

    Have all products display on main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+  	@products = Product.all
+  	@orders = Order.all 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305164941) do
+ActiveRecord::Schema.define(version: 20160312034435) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "order_id"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20160305164941) do
     t.string   "email"
     t.string   "name"
     t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
