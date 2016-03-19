b4cb0765c5f3adeada31c8fb7f9e2f384dde09b1
SheaMeyers <shea.meyers@gmail.com>
2016-03-19 17:04:10 -0600

    Calculating subtotal of items in shopping cart

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,9 +2,15 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org
 
-var myFunction = function(productName, productPrice) {
-	//console.log(productName + ' ' + productPrice);
+var addItemToCart = function(productName, productPrice) {
 	var newItem = document.createElement('li');
 	newItem.innerHTML = productName + "    $" + productPrice;
+	calculateSubtotal(productPrice);
 	document.getElementById('itemList').appendChild(newItem);
+}
+
+var calculateSubtotal = function(price) {
+	var currentTotal = parseFloat(document.getElementById('subTotalAmount').innerHTML);
+	currentTotal = currentTotal + parseFloat(price);
+	document.getElementById('subTotalAmount').innerHTML = currentTotal;
 }
