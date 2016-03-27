00f7d10119ac9428d8687832db333c876fba0602
SheaMeyers <shea.meyers@gmail.com>
2016-03-26 23:15:41 -0600

    Refactored how current orders are being added and shown on html page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,10 @@ class WelcomeController < ApplicationController
   def index
   	@products = Product.all
   	@users = User.where(email: 'test@email.com')
-  	@orders = Order.where(email: 'test@email.com')
-  	# @product_orders = []
-  	# orders.each do |order| 
-  	# 	@product_orders << Product.where(title: order.product_id)
-  	# end
+  	orders = Order.where(email: 'test@email.com')
+  	@product_orders = []
+  	orders.each do |order| 
+  		@product_orders = @product_orders + Product.where(title: order.product_id).to_a
+  	end
   end
 end
