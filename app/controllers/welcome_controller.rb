74125a78cf46bb75a0cea7652f70265832f0385c
SheaMeyers <shea.meyers@gmail.com>
2016-03-27 16:29:57 -0600

    Have subtotal being added and displayed on page load

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,5 +7,9 @@ class WelcomeController < ApplicationController
   	orders.each do |order| 
   		@product_orders = @product_orders + Product.where(title: order.product_id).to_a
   	end
+  	@product_total = 0.00
+  	@product_orders.each do |product_order|
+  		@product_total = @product_total + product_order.price.to_f
+	end
   end
 end
