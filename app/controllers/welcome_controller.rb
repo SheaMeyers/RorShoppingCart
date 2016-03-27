c2cfa917a1d36a543939449046c3f409d807bc4d
SheaMeyers <shea.meyers@gmail.com>
2016-03-26 23:07:15 -0600

    Showing currently stored orders on page load

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < ApplicationController
   def index
   	@products = Product.all
   	@users = User.where(email: 'test@email.com')
+  	@orders = Order.where(email: 'test@email.com')
+  	# @product_orders = []
+  	# orders.each do |order| 
+  	# 	@product_orders << Product.where(title: order.product_id)
+  	# end
   end
 end
